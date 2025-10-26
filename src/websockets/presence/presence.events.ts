632defe24ca2c7ae11ee27423f25697dd7a14b12
Gui97p <guipassaroto07@gmail.com>
2025-10-25 22:21:30 -0400

    Quick feature: presence sync

--- src/websockets/presence/presence.events.ts
+++ src/websockets/presence/presence.events.ts
@@ -1,5 +1,7 @@
 import { Server, Socket } from "socket.io"
 
+const onlineUsers = new Set<string>();
+
 export async function registerPresenceEvents(server: Server, socket: Socket) {
     
 }
@@ -10,10 +12,11 @@ export async function onConnectPresence(server: Server, socket: Socket) {
     const sockets = await server.in(`user:${userId}`).fetchSockets()
 
     if (sockets.length <= 1) {
+        onlineUsers.add(userId)
         server.to('channel:global').emit('presence:online', { userId })
-    } else {
-        socket.emit('presence:online', { userId })
     }
+
+    socket.emit('presence:sync', { onlineUsers: Array.from(onlineUsers) })
 }
 
 export async function onDisconnectPresence(server: Server, socket: Socket) {
@@ -22,6 +25,7 @@ export async function onDisconnectPresence(server: Server, socket: Socket) {
     const sockets = await server.in(`user:${userId}`).fetchSockets()
 
     if (sockets.length === 0) {
+        onlineUsers.delete(userId);
         server.to('channel:global').emit('presence:offline', { userId })
     }
 }
