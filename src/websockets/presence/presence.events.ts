0f94ea48d5a04711de3fe4a89430d42f57fb59eb
Gui97p <guipassaroto07@gmail.com>
2025-10-30 11:30:05 -0400

    refactor: Presence now emits only presence:sync for channel

--- src/websockets/presence/presence.events.ts
+++ src/websockets/presence/presence.events.ts
@@ -1,55 +1,54 @@
-import { Server, Socket } from "socket.io"
-import { findUserById } from '../../modules/user/user.service'
-import { User } from "@prisma/client";
+import { Server, Socket } from 'socket.io';
+import { findUserById } from '../../modules/user/user.service';
+import { User } from '@prisma/client';
 
 const onlineUsers = new Set<string>();
 
 async function getUserData(userIdArray: string[]): Promise<Partial<User>[]> {
-    const userArray: Partial<User>[] = []
-    
-    for (let c = 0; c < userIdArray.length; c++) {
-        const user = await findUserById(userIdArray[c])
-        if (!user) continue
-        userArray.push({
-            id: user.id,
-            name: user.name,
-            displayName: user.displayName,
-            avatar: user.avatar
-        })
-    }
-    
-    return userArray;
+  const userArray: Partial<User>[] = [];
+
+  for (let c = 0; c < userIdArray.length; c++) {
+    const user = await findUserById(userIdArray[c]);
+    if (!user) continue;
+    userArray.push({
+      id: user.id,
+      name: user.name,
+      displayName: user.displayName,
+      avatar: user.avatar,
+    });
+  }
+
+  return userArray;
 }
 
 export async function registerPresenceEvents(server: Server, socket: Socket) {
-    socket.on('presence:sync', async () => {
-        const userData = await getUserData(Array.from(onlineUsers))
-        socket.emit('presence:sync', { onlineUsers: userData })
-    })
+  socket.on('presence:sync', async () => {
+    const userData = await getUserData(Array.from(onlineUsers));
+    socket.emit('presence:sync', { onlineUsers: userData });
+  });
 }
 
 export async function onConnectPresence(server: Server, socket: Socket) {
-    const userId = socket.data.userId
+  const userId = socket.data.userId;
 
-    const sockets = await server.in(`user:${userId}`).fetchSockets()
-    
-    if (sockets.length <= 1) {
-        onlineUsers.add(userId)
-        const user = await findUserById(userId)
-        server.to('channel:global').emit('presence:online', user)
-    }
-    
-    const userData = await getUserData(Array.from(onlineUsers))
-    socket.emit('presence:sync', { onlineUsers: userData })
+  const sockets = await server.in(`user:${userId}`).fetchSockets();
+
+  if (sockets.length <= 1) {
+    onlineUsers.add(userId);
+  }
+
+  const userData = await getUserData(Array.from(onlineUsers));
+  server.to('channel:global').emit('presence:sync', { onlineUsers: userData });
 }
 
 export async function onDisconnectPresence(server: Server, socket: Socket) {
-    const userId = socket.data.userId
+  const userId = socket.data.userId;
 
-    const sockets = await server.in(`user:${userId}`).fetchSockets()
+  const sockets = await server.in(`user:${userId}`).fetchSockets();
 
-    if (sockets.length === 0) {
-        onlineUsers.delete(userId);
-        server.to('channel:global').emit('presence:offline', { userId })
-    }
+  if (sockets.length === 0) {
+    onlineUsers.delete(userId);
+    const userData = await getUserData(Array.from(onlineUsers));
+    server.to('channel:global').emit('presence:sync', { onlineUsers: userData });
+  }
 }
